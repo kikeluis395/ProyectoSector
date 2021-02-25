891a9995d8fc895494a0d2791279a7b01ec5b6d2
Luis Alvarado <kikeluis395@gmail.com>
2021-02-25 02:41:53 -0500

    cambios finales

--- src/components/footerNav/FooterNav.js
+++ src/components/footerNav/FooterNav.js
@@ -3,12 +3,9 @@ import React from 'react';
 function FooterNav({footerNav}) {
   const {item} = footerNav
   return (
-    
-    <div>
       <a href="a">
         <li>{item}</li>
       </a>
-    </div>
   );
 }
 
